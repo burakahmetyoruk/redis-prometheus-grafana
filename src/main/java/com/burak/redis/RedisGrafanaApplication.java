a218c0036839d3d8935227748cd930f40f41e57c
Burak Yoruk <burak.yoruk@turkcell.com.tr>
2018-06-19 09:00:21 +0300

    Add Redis Prometheus Grafana Docker file

---
+++ src/main/java/com/burak/redis/RedisGrafanaApplication.java
@@ -0,0 +1,12 @@
+package com.burak.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisGrafanaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/burak/redis/config/RedisConfig.java
@@ -0,0 +1,7 @@
+package com.burak.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+}
